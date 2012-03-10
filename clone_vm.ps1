08eed8dc96cc9e6b136a0c4ff4b221fc12eb4696
Carlos Perez <carlos_perez@darkoperator.com>
2012-03-09 22:32:56 -0400

    a couple of fixes, u=indentation and version number

--- clone_vm.ps1
+++ clone_vm.ps1
@@ -6,9 +6,10 @@
 # Usage:            To load this module in your Script Editor:                                                        #
 #                   1. Open PowerCli shell.                                                                           #
 #                   2. Navigate to where the script is located.                                                       #
-#                   3. Call script with options. .\clone_vm.ps1 -Server <ESX or vCenter> -VM <VM to Clone> -CloneName #
+#                   3. Call script with options. .\clone_vm.ps1 -Server <ESX> -VM <VM to Clone> -CloneName            #
 #                      <Name to give to Clone VM> <Enter>                                                             #
 # License:			BSD 3 Clause License                                                                              #
+# Version:          0.2                                                                                               #
 #######################################################################################################################
 # Define Script Parameters
 param 
@@ -38,7 +39,8 @@ if ( (Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue
 	Write-Host "[*] VMware Automation Snapin is not loaded, attempting to load..." -ForegroundColor Green
     Try {
 		Add-PsSnapin VMware.VimAutomation.Core | Out-Null
-	} Catch {
+	} 
+	Catch {
 		$Host.UI.WriteErrorLine("[-] Could not load snapping check that PowerCli is installed.")
 		exit
 	}
@@ -68,18 +70,21 @@ if ( (Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue
 function find_vm {
 	param($vm_name)
 	$vm_info =@{}
-	$orig_vm = Get-VM | where {$_.name -match $vm_name}
+	$orig_vm = Get-VM | where {$_.name -match $vm_name} | select -First 1
 
 	if ($orig_vm -ne $null){
 		if ($orig_vm.PowerState -eq "PoweredOff"){
 			$vm_info_raw = $orig_vm.Extensiondata.Summary.Config.VmPathName.Split(" ")
 			$vm_info['datastore'] = $vm_info_raw[0] -creplace "\[|\]", ""
 			$vm_info['folder'] = $vm_info_raw[1].split("/")[0]
+			$VM_info['vmhost'] = $orig_vm.VMHost
 		}
 		else {
 			$Host.UI.WriteErrorLine("[-] VM must be powered off before attempting to clone")
+			exit
 		}
-	}else {
+	}
+	else {
 		$Host.UI.WriteErrorLine(" [-] Could not find the VM specified")
 		exit
 	}
@@ -89,9 +94,9 @@ function find_vm {
 function clone {
 	param($dsstore, $fs_folder, $clone_name, $vmhost)
 	# Generate PSDrive
-	$datastore = Get-Datastore $dsstore
+	$datastore = Get-Datastore $dsstore 
 	# Check if a previous PSDrive exists
-	$psd = Get-PSDrive | where {$_.name -eq "dd"}
+	$psd = Get-PSDrive | where {$_.name -eq "vmds"}
 	if ($psd -eq $null){ 
 		$ds_path = New-PSDrive -Location $datastore -Name vmds -PSProvider VimDatastore -Root "\"
 	}
@@ -140,6 +145,6 @@ if ($connected) {
 	Write-Host "[*] Cloning $VM as $CloneName" -ForegroundColor Green
 	check_vm $CloneName
 	$vmhash = find_vm $VM
-	clone $vmhash['datastore'] $vmhash['folder'] $CloneName $Server
+	clone $vmhash['datastore'] $vmhash['folder'] $CloneName $vmhash['vmhost']
 	change_uuid $CloneName
 }
